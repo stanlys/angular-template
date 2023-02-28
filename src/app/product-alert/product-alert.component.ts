292d2394061d2d8f69089113ec6a0784b5f813fc
Stanlys <stanlys@yandex.ru>
2023-02-28 12:05:41 +0300

    feat: add EventEmmiter

--- src/app/product-alert/product-alert.component.ts
+++ src/app/product-alert/product-alert.component.ts
@@ -1,11 +1,14 @@
-import { Component, Input } from '@angular/core';
-import { Product } from '../products';
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Product } from "../products";
 
 @Component({
-  selector: 'app-product-alert',
-  templateUrl: './product-alert.component.html',
-  styleUrls: ['./product-alert.component.css'],
+  selector: "app-product-alert",
+  templateUrl: "./product-alert.component.html",
+  styleUrls: ["./product-alert.component.css"],
 })
-export class ProductAlertComponent {
+export class ProductAlertComponent implements OnInit {
   @Input() product!: Product;
+  @Output() notify = new EventEmitter();
+
+  ngOnInit() {}
 }
