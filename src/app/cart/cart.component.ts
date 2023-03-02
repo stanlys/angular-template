4d32fbc916d083984d86a1b4de5d836b8d2cf1b4
Stanlys <stanlys@yandex.ru>
2023-03-02 16:57:43 +0300

    feat: correct formBuilder

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { CartService } from "../cart.service";
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: "app-cart",
@@ -9,12 +10,24 @@ import { CartService } from "../cart.service";
 export class CartComponent {
   products = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {
+  checkoutForm = this.form.group({
+    name: "",
+    address: "",
+    products: Array<string>,
+  });
+
+  constructor(private cartService: CartService, private form: FormBuilder) {
     this.products = this.cartService.getItems();
   }
 
   clearCart() {
     this.products = this.cartService.clearCart();
     console.log("clear");
+  }
+
+  onSubmit(): void {
+    this.products = this.cartService.clearCart();
+    console.warn("You order submit", this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 }
