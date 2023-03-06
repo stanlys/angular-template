de88bcaefe3fc263b73aa6a94cee6330d6930be7
Stanlys <3995782+stanlys@users.noreply.github.com>
2023-03-06 20:53:38 +0300

    feat: correct modal window

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "app-modal",
@@ -6,6 +6,7 @@ import { Component } from "@angular/core";
   styleUrls: ["./modal.component.css"],
 })
 export class ModalComponent {
+  @Input() title: string;
   isShow = true;
 
   toggleShow() {
