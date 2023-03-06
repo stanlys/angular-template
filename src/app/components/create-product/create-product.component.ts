05ae2ad7a1d77e6591c836e1ea38415edb6231ef
Stanlys <3995782+stanlys@users.noreply.github.com>
2023-03-06 21:45:09 +0300

    feat: correct modal window

--- src/app/components/create-product/create-product.component.ts
+++ src/app/components/create-product/create-product.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-create-product',
-  templateUrl: './create-product.component.html',
-  styleUrls: ['./create-product.component.css']
+  selector: "app-create-product",
+  templateUrl: "./create-product.component.html",
+  styleUrls: ["./create-product.component.css"],
 })
-export class CreateProductComponent {
+export class CreateProductComponent implements OnInit {
+  form = new FormGroup({
+    title: new FormControl<string>("", [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    price: new FormControl<number>(0, [
+      Validators.required,
+      Validators.min(10),
+    ]),
+  });
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  get isValid() {
+    // if (this.form.touched) return false;
+    return this.form.status === "VALID";
+  }
+
+  submit() {
+    if (this.isValid) console.warn("Add---");
+  }
 }
