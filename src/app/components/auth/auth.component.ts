9977c88c589e61c3fb54eca9566daddbf1550a25
Stanlys <3995782+stanlys@users.noreply.github.com>
2023-03-08 19:59:29 +0300

    feat: correct auth module

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,5 +1,15 @@
 import { Component } from "@angular/core";
 
+interface IPassword {
+  type: string;
+  src: string;
+}
+
+const passwordElements: IPassword[] = [
+  { type: "password", src: "../../../assets/view.png" },
+  { type: "text", src: "../../../assets/hide.png" },
+];
+
 @Component({
   selector: "app-auth",
   templateUrl: "./auth.component.html",
@@ -8,6 +18,13 @@ import { Component } from "@angular/core";
 export class AuthComponent {
   loginText = "Окно входа";
   singUpText = "Окно регистрации";
-
+  password = passwordElements[0];
   isAuth = true;
+
+  togglePasswordElement() {
+    this.password =
+      this.password.type === "password"
+        ? passwordElements[1]
+        : passwordElements[0];
+  }
 }
